8d7abad5f66c9ad87fb5423f48c1df9612a3b26a
qegabbe <qegabbe@outlook.de>
2016-04-26 17:44:03 +0200

    Update g_context.h

--- docs/sample001/g_context.h
+++ docs/sample001/g_context.h
@@ -1,45 +1,4 @@
-/********************************************************************************
-**
-**  CONFIDENTIAL - GETRAG Getriebe- und Zahnradfabrik
-**                 Hermann Hagenmeyer GmbH & Cie KG
-** 
-**  This is an unpublished work, which is a trade secret, created in 2013.      
-**  GETRAG Getriebe- und Zahnradfabrik Hermann Hagenmeyer GmbH & Cie KG
-**  owns all rights to this work and intends to maintain it in confidence
-**  to preserve its trade secret status. GETRAG Getriebe- und Zahnradfabrik
-**  Hermann Hagenmeyer GmbH & Cie KG reserves the right to protect this work       
-**  as an unpublished copyrighted work in the event of an inadvertent or      
-**  deliberate unauthorized publication. GETRAG Getriebe- und Zahnradfabrik
-**  Hermann Hagenmeyer GmbH & Cie KG also reserves its rights under the copyright
-**  laws to protect this work as a published work. Those having access to this
-**  work may not copy it, use it or disclose the information contained in it
-**  without the written authorization of GETRAG Getriebe- und Zahnradfabrik
-**  Hermann Hagenmeyer GmbH & Cie KG.
-**
-*******************************************************************************/
 
-/***************************************************************************//**
-*
-*     \file            getrag_context.h
-*
-*     \author          Tim Weyres
-*
-*     \date            22.05.2013
-*
-*     \b Compiler:     Renesas V9.04
-*
-*     \b Description:  Getrag context header for general defines and typedefs.
-*
-*     <b> Modification History:   </b>
-*
-*      Date        |    Name        |                                         \n
-*    --------------+----------------+-----------------------------------------\n
-*    22.05.2013    | Tim Weyres     | initial revision                        \n
-*    --------------+----------------+-----------------------------------------\n
-*                  |                |                                         \n
-*    --------------+----------------+-----------------------------------------
-*
-*******************************************************************************/
 
 /*******************************************************************************
 *
