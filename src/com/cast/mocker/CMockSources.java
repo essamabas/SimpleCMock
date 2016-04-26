3f4947d0b0ace1c3e998d2c840d75be5d252c9d8
qegabbe <qegabbe@outlook.de>
2016-04-26 17:14:18 +0200

    Update CMockSources.java

--- src/com/cast/mocker/CMockSources.java
+++ src/com/cast/mocker/CMockSources.java
@@ -6,6 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import javax.swing.JFileChooser;
+
+import org.eclipse.core.runtime.CoreException;
+
 import logger.MyLogger;
 
 public class CMockSources {
@@ -13,32 +17,24 @@ public class CMockSources {
 	private final static Logger logger = MyLogger.getLogger();
 	
 	public CMockSources() {
-		// TODO Auto-generated constructor stub
+		// TODO Auto-generated constructor stub		
 	}
 
-	public static void main(String[] args) {
+    /**
+    * Mock Files in Passed Folder
+    * @return boolean flag as an indication of successful operation
+    * @param	Files	Folder    
+    * @param 	includePaths	include paths to be scanned with the source file
+    * @throws CoreException
+    */	
+	public List<String> MockFiles(String folderpath, String[] includePaths) {
 		
-		// Initialise Logger
-		try {
-			MyLogger.setup();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		// get list of Files in Passed Folder
-		
-		// Set includePaths
-		String[] includePaths = new String[1];
-		includePaths[0] = "docs/sample001";
-		
-		//
+		// List of mocked-files
 		List<String> results = new ArrayList<String>();
-		File[] files = new File("docs/sample001").listFiles();
-		//If this pathname does not denote a directory, then listFiles() returns null. 
+		File[] files = new File(folderpath).listFiles();
+		
 		for (File file : files) {
 		    if (file.isFile()) {
-		        file.getAbsolutePath();
 		        // Mock only C-Files
 		        if(file.getName().endsWith(".c")) {
 		        	results.add(file.getName());
@@ -46,9 +42,29 @@ public class CMockSources {
 		        	CMockerCreator MyCMocker = new CMockerCreator();
 		        	MyCMocker.CreateMockPrepressor(file.getAbsolutePath(), includePaths);
 		        }
-		        
 		    }
 		}
+		return results;
+	}
+	
+	public static void main(String[] args) {
+
+		// Initialise Logger
+		try {
+			MyLogger.setup();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		// get list of Files in Passed Folder
+		String folderpath = "docs/sample001";
+		
+		// Set includePaths
+		String[] includePaths = new String[1];
+		includePaths[0] = "docs/sample001";
+		//
+		CMockSources cm = new CMockSources();
+		List<String> results = cm.MockFiles(folderpath, includePaths);
 	}
 
 }
